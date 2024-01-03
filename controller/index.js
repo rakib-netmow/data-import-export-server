6bbf3d94e2214dfd218beedf1cfcd0ed1f3c9679
rakib-netmow <rakib.netmow@gmail.com>
2024-01-03 17:51:30 +0600

    return keys

--- controller/index.js
+++ controller/index.js
@@ -1,6 +1,15 @@
+const csvtojsonV2 = require("csvtojson/v2");
 const importData = async (req, res) => {
   const file = req.file?.path;
-  console.log("Imported!");
+  const csvFilePath = file;
+  const jsonArray = await csvtojsonV2().fromFile(csvFilePath);
+  // console.log("Imported!", jsonArray);
+  const finalData = [];
+  jsonArray.map((e) =>
+    Object.keys(e).map((k) => !finalData.includes(k) && finalData.push(k))
+  );
+
+  res.send(finalData);
 };
 
 module.exports = {
